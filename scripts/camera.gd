26e564544a9aad0bd9590e07cee583a796c0a24c
Benjamin Massey <benjamin.w.massey@gmail.com>
2025-03-04 22:41:06 -0800

    camera script is a bit more real now, and player script is cleaned up for (hopefully) cleaner readability

--- scripts/camera.gd
+++ scripts/camera.gd
@@ -1,7 +1,12 @@
 extends Camera3D
 
 @export var player: Node3D;
+@export var distance: float = 5.0;
 
 func _process(delta: float) -> void:
-	rotation = player.rotation;
-	look_at(player.global_position);
+	var p_pos = player.global_position;
+	var w_pos = Vector3.ZERO;
+	var w_p_line = (p_pos - w_pos).normalized();
+	position = p_pos + w_p_line * distance;
+	
+	look_at(p_pos, player.forward_vec * Quaternion(w_p_line, 1.5 * PI));
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,19 +1,17 @@
 extends Node3D
 
-var world_radius: float = 7.5
-var player_radius: float = 1.0
-var player_position: Vector3 = Vector3(0, world_radius + (player_radius * 0.5), 0)
+var world_radius: float = 7.5;
+var player_radius: float = 1.0;
+var player_position: Vector3 = Vector3(0, world_radius + (player_radius * 0.5), 0);
 var forward_vec: Vector3 = Vector3.RIGHT;
-var rotation_speed: float = 0.5
-var turn_speed: float = 1.5
+var rotation_speed: float = 0.5;
+var turn_speed: float = 1.5;
 
 func _process(delta: float) -> void:
-	var forward_quat: Quaternion = Quaternion(forward_vec, rotation_speed * delta)
-	player_position = forward_quat * player_position
-	if Input.is_action_pressed("turn_left"):
-		var turn_quat: Quaternion = Quaternion(Vector3.UP, turn_speed * delta)
-		forward_vec = turn_quat * forward_vec
 	if Input.is_action_pressed("turn_right"):
-		var turn_quat: Quaternion = Quaternion(Vector3.UP, -turn_speed * delta)
-		forward_vec = turn_quat * forward_vec
-	transform.origin = player_position
+		forward_vec *= Quaternion(Vector3.UP, turn_speed * delta);
+	if Input.is_action_pressed("turn_left"):
+		forward_vec *= Quaternion(Vector3.UP, -turn_speed * delta);
+	player_position *= Quaternion(forward_vec, rotation_speed * delta);
+	transform.origin = player_position;
+	look_at(Quaternion(forward_vec, rotation_speed * delta) * player_position);
