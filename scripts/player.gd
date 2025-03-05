15f00edd94a2f440b05196ad206327ad5e309661
Benjamin Massey <benjamin.w.massey@gmail.com>
2025-03-04 22:46:25 -0800

    fixed player axis behavior by calculating correct line for turning

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,5 +1,6 @@
 extends Node3D
 
+var world_position: Vector3 = Vector3.ZERO;
 var world_radius: float = 7.5;
 var player_radius: float = 1.0;
 var player_position: Vector3 = Vector3(0, world_radius + (player_radius * 0.5), 0);
@@ -8,10 +9,11 @@ var rotation_speed: float = 0.5;
 var turn_speed: float = 1.5;
 
 func _process(delta: float) -> void:
+	var world_intersection = (global_position - world_position).normalized();
 	if Input.is_action_pressed("turn_right"):
-		forward_vec *= Quaternion(Vector3.UP, turn_speed * delta);
+		forward_vec *= Quaternion(world_intersection, turn_speed * delta);
 	if Input.is_action_pressed("turn_left"):
-		forward_vec *= Quaternion(Vector3.UP, -turn_speed * delta);
+		forward_vec *= Quaternion(world_intersection, -turn_speed * delta);
 	player_position *= Quaternion(forward_vec, rotation_speed * delta);
 	transform.origin = player_position;
 	look_at(Quaternion(forward_vec, rotation_speed * delta) * player_position);
